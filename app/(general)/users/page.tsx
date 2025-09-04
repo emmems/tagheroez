4250a14e9c62668896066a9463f6b664acfa1ef0
kamil713 <kam.pietrz@gmail.com>
2025-09-04 12:24:07 +0200

    feat: extend reusable component about btn

--- app/(general)/users/page.tsx
+++ app/(general)/users/page.tsx
@@ -1,10 +1,16 @@
 import SectionTitle from "@/components/section-title"
+import { Button } from "@/components/ui/button"
 
 function UserManagementPage() {
   return (
     <SectionTitle
       title="User Management"
       subtitle="Manage internal users and their access rights"
+      button={
+        <Button size="lg">
+          Add New User
+        </Button>
+      }
     />
   )
 }
--- components/section-title.tsx
+++ components/section-title.tsx
@@ -1,13 +1,17 @@
 interface SectionTitleProps {
   title: string;
   subtitle: string;
+  button?: React.ReactNode;
 }
 
-function SectionTitle({ title, subtitle } : SectionTitleProps) {
+function SectionTitle({ title, subtitle, button } : SectionTitleProps) {
   return (
-    <section>
-      <h3 className="text-3xl font-bold">{title}</h3>
-      <p className="text-base font-medium text-gray-600">{subtitle}</p>
+    <section className="flex justify-between">
+      <div>
+        <h3 className="text-3xl font-bold">{title}</h3>
+        <p className="text-base font-medium text-gray-600">{subtitle}</p>
+      </div>
+      {button && <div>{button}</div>}
     </section>
   )
 }
